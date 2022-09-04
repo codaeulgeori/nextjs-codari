9eef4fcbe5d879c0624bcfdcccff8684bf729df3
Gunn Kim <79524742+gunnyk@users.noreply.github.com>
2022-09-05 05:24:02 +0900

    Initialize project using Create Next App

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+const HomePage = () => {
+  return (
+    <div>
+      <Head>
+        <title>코다리</title>
+      </Head>
+      코다을거리
+    </div>
+  );
+};
+
+export default HomePage;
